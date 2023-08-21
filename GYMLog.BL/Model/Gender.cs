16bd3e250f8d5c3466fd3970a09f8a4b448236a8
McConderEz <kamazitun.jaykehd@gmail.com>
2023-08-22 00:15:58 +0300

    Создана модель Пользователя

---
+++ GYMLog.BL/Model/Gender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GYMLog.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name">Имя пола.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя поля не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ GYMLog.BL/Model/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GYMLog.BL.Model
+{
+    public class User
+    {
+        #region Свойства
+        public string Login { get; }
+        public Gender Gender { get; }
+        public DateTime BirthDate { get; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        #endregion
+
+        public User(string login, 
+            Gender gender, 
+            DateTime birthDate, 
+            double weight, 
+            double height)
+        {
+            #region Проверка условий
+            if (string.IsNullOrWhiteSpace(login))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или null", nameof(login));
+            }
+
+            if(Gender == null)
+            {
+                throw new ArgumentNullException("Гендер не может быть null",nameof(gender));
+            }
+
+            if(birthDate < DateTime.Parse("01.01.1910") && birthDate >= DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException("Выход за границы допустимых значений",nameof(birthDate));
+            }
+
+            if(weight <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Выход за границы допустимых значений", nameof(weight));
+            }
+
+            if(height <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Выход за границы допустимых значений", nameof(height));
+            }
+            #endregion
+
+            Login = login;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight; 
+            Height = height;
+
+        }
+
+        public override string ToString()
+        {
+            return Login;
+        }
+    }
+}
